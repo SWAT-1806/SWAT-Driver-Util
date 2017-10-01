f349d051b5776bda06ae7f1b07395d0addc2a4d7
Chris Ariagno <christopherariagno@gmail.com>
2017-10-01 17:53:43 -0500

    Added a whole lot more stuff to it

---
+++ ui.js
@@ -0,0 +1,25 @@
+let ui = {
+    autoSelect: document.getElementById('auto-select'),
+};
+NetworkTables.addKeyListener('/SmartDashboard/time_running', (key, value) => {
+    // Clear previous list
+    while (ui.autoSelect.firstChild) {
+        ui.autoSelect.removeChild(ui.autoSelect.firstChild);
+    }
+    // Make an option for each autonomous mode and put it in the selector
+    for (let i = 0; i < value.length; i++) {
+        var option = document.createElement('option');
+        option.appendChild(document.createTextNode(value[i]));
+        ui.autoSelect.appendChild(option);
+    }
+    // Set value to the already-selected mode. If there is none, nothing will happen.
+    ui.autoSelect.value = NetworkTables.getValue('/SmartDashboard/currentlySelectedMode');
+});
+
+// Load list of prewritten autonomous modes
+NetworkTables.addKeyListener('/SmartDashboard/autonomous/selected', (key, value) => {
+    ui.autoSelect.value = value;
+});
+ui.autoSelect.onchange = function () {
+    NetworkTables.putValue('/SmartDashboard/autonomous/selected', this.value);
+};
